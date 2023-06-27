27dc1f66b1f8df2896ee4c7bc44f5e97eb0df8d2
rkamysz <radoslaw.kamysz@gmail.com>
2023-06-27 11:55:24 +0200

    update dependencies

--- src/processor/processors/dao-worlds/dao-worlds.delta-processor.ts
+++ src/processor/processors/dao-worlds/dao-worlds.delta-processor.ts
@@ -4,6 +4,6 @@ import { ProcessorTaskModel, log } from '@alien-worlds/history-tools-common';
 export class DaoWorldsDeltaProcessor extends DeltaProcessor<unknown> {
   public async run(model: ProcessorTaskModel): Promise<void> {
     log('Hello World from delta processor!');
-    log(model);
+    this.reject();
   }
 }
--- src/processor/processors/dao-worlds/dao-worlds.trace-processor.ts
+++ src/processor/processors/dao-worlds/dao-worlds.trace-processor.ts
@@ -4,6 +4,6 @@ import { ProcessorTaskModel, log } from '@alien-worlds/history-tools-common';
 export class DaoWorldsTraceProcessor extends ActionTraceProcessor {
   public async run(model: ProcessorTaskModel): Promise<void> {
     log('Hello World from trace processor!');
-    log(model);
+    this.reject();
   }
 }
